578015b0f0bb2843e780ba52a376544f455a56de
johndward01 <johndward01@gmail.com>
2023-07-17 11:17:28 -0500

    Add project files.

---
+++ Inheritance_Exercise_01/Program.cs
@@ -0,0 +1,10 @@
+﻿// TODO: Create a base class called ComputerSystem
+// TODO: Create 4 properties called Processor, RAM, GPU, and OS 
+
+// TODO: Create 3 derived classes called Desktop, Laptop, and Server
+// TODO: Give each derived class 1 unique property (i.e. HasTouchScreen)
+// TODO: Create a method that creatively displays the derived classes information called DisplaySystemInfo()
+
+// TODO: Create objects for each of the derived classes and give their properties values
+
+// TODO: Creatively display the information to the console
